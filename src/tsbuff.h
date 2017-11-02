e9131dccf18c27e0c560f46c833bf6d37d455c7f
epgdatacapbon <sstream00@yahoo.co.jp>
2017-11-02 09:46:01 +0900

    Increase TS_BufSize
    https://hayabusa6.5ch.net/test/read.cgi/linux/1487503115/703

--- src/tsbuff.h
+++ src/tsbuff.h
@@ -5,8 +5,8 @@
 */
 #pragma once
 
-//# TS Buffer size = 3584 KiB
-#define TS_BufSize  3670016
+//# TS Buffer size = 30 MB
+#define TS_BufSize  31457280
 
 //# max number of submitted IO requests
 #define TS_MaxNumIO  20
